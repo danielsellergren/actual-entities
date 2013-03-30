ed21b955e1e2185e181b5f821b9196312b355656
Daniel Sellergren <danielsellergren@gmail.com>
2013-03-30 18:05:36 -0400

    Add header element

--- header.php
+++ header.php
@@ -6,12 +6,16 @@
 </head>
 <body>
 
-    <h1 id="site-title"><a href="/">Actual Entities</a></h1>
+    <header>
 
-    <nav id="main-nav">
-        <ul>
-            <li><a href="/history">History</a></li>
-            <li><a href='/ai'>Artificial Intelligence</a></li>
-            <li><a href="/demo">Demo</a></li>
-        </ul>
-    </nav>
+        <h1 id="site-title"><a href="/">Actual Entities</a></h1>
+
+        <nav id="main-nav">
+            <ul>
+                <li><a href="/history">History</a></li>
+                <li><a href='/ai'>Artificial Intelligence</a></li>
+                <li><a href="/demo">Demo</a></li>
+            </ul>
+        </nav>
+
+    </header>
