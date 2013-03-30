72323e956b3b89c4e3113c0db17aa6a92f106142
Daniel Sellergren <danielsellergren@gmail.com>
2013-03-30 18:41:25 -0400

    Add #container and Google Fonts

--- header.php
+++ header.php
@@ -2,20 +2,24 @@
 <html>
 <head>
     <title>Actual Entities and Artificial Intelligence</title>
-    <link type="text/css" rel="stylesheet" href="css/style.css" />
+    <link href='http://fonts.googleapis.com/css?family=Alfa+Slab+One' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Noto+Serif:400,700,400italic' rel='stylesheet' type='text/css'>
+    <link type="text/css" rel="stylesheet" href="/css/style.css" />
 </head>
 <body>
 
-    <header>
+    <div id="container">
 
-        <h1 id="site-title"><a href="/">Actual Entities</a></h1>
+        <header>
 
-        <nav id="main-nav">
-            <ul>
-                <li><a href="/history">History</a></li>
-                <li><a href='/ai'>Artificial Intelligence</a></li>
-                <li><a href="/demo">Demo</a></li>
-            </ul>
-        </nav>
+            <h1 id="site-title"><a href="/">Actual Entities</a></h1>
 
-    </header>
+            <nav id="main-nav">
+                <ul>
+                    <li><a href="/history">History</a></li>
+                    <li><a href='/ai'>Artificial Intelligence</a></li>
+                    <li><a href="/demo">Demo</a></li>
+                </ul>
+            </nav>
+
+        </header>
