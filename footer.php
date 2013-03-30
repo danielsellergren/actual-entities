92f96f393bfb5de7d9d7f6765a6d1ba6046e91d0
Daniel Sellergren <danielsellergren@gmail.com>
2013-03-30 19:29:30 -0400

    Add links to footer

--- footer.php
+++ footer.php
@@ -1,3 +1,12 @@
+    <footer>
+
+        <nav id="footer-nav">
+            <a href="http://danielsellergren.com">Author</a>
+            <a href="/references">References</a>
+        </nav>
+
+    </footer>
+
     </div>
 
 </body>
