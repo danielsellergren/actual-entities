861db94b2a8e01068d17cc0ec983ca2c68b6ef2b
Daniel Sellergren <danielsellergren@gmail.com>
2013-03-30 17:45:15 -0400

    Add basic site globals

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Actual Entities and Artificial Intelligence</title>
+</head>
+<body>
+
+    <h1 id="site-title"><a href="/">Actual Entities</a></h1>
+
+    <nav id="main-nav">
+        <ul>
+            <li><a href="/history">History</a></li>
+            <li><a href='/ai'>Artificial Intelligence</a></li>
+            <li><a href="/demo">Demo</a></li>
+        </ul>
+    </nav>
