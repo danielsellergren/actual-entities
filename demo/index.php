4f66d6281f32d33155fdebbadf117f299b9cd8ad
Daniel Sellergren <danielsellergren@gmail.com>
2013-03-31 19:50:59 -0400

    Basic layout for demo

--- demo/index.php
+++ demo/index.php
@@ -1,5 +1,22 @@
-<?php require('../header.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Actual Entities and Artificial Intelligence | Demo</title>
+    <link href='http://fonts.googleapis.com/css?family=Alfa+Slab+One' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Noto+Serif:400,700,400italic' rel='stylesheet' type='text/css'>
+    <link type="text/css" rel="stylesheet" href="/css/demo.css" />
+</head>
+<body>
 
-    <h2>Actual Entities Demo</h2>
+    <aside id="sidebar">
+        <h1 id="site-title"><a href="/">Actual Entities</a></h1>
+        <div id="controls">
+        </div>
+    </aside>
 
-<?php require('../footer.php'); ?>
+    <section id="universe">
+
+    </section>
+
+</body>
+</html>
