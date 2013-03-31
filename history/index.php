eb7e31d48e7cecf7c6d49c591398d018aa499264
Daniel Sellergren <danielsellergren@gmail.com>
2013-03-31 15:33:42 -0400

    Add first few lines of history section

--- history/index.php
+++ history/index.php
@@ -2,4 +2,10 @@
 
     <h2>History of Actual Entities</h2>
 
+    <p>Actual Entities are the fundamental elements of reality in the philosophy of English-American mathematician and logician, <a href="http://en.wikipedia.org/wiki/Alfred_North_Whitehead">Alfred North Whitehead</a>.</p>
+
+    <img src="../img/whitehead.jpg" width="285" height="320" alt="Alfred North Whitehead" />
+
+    <p>Whitehead is best known for his philosophical treatise <a href="http://en.wikipedia.org/wiki/Process_and_Reality">Process and Reality</a>, and for coauthoring the magnificent <a href="http://en.wikipedia.org/wiki/Principia_Mathematica">Principia Mathematica</a> with Bertrand Russell.</p>
+
 <?php require('../footer.php'); ?>
