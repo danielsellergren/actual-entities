e8d31fa6c56c4ad08d881683ddc51e3530ee789b
Daniel Sellergren <danielsellergren@gmail.com>
2013-11-10 15:27:18 -0500

    Initial references page commit

---
+++ references/index.php
@@ -0,0 +1,12 @@
+<?php require('../header.php'); ?>
+
+<h2>References</h2>
+
+	<ol>
+		<li>Hooper, S. E., "Whitehead's Philosophy: Actual Entities", Philosophy, Vol 16, No. 63, pp. 285-305.</li>
+		<li>Pidaparti, R. M., Primeaux, D., & Saunders, B., "Modeling and Simulation of Nanoscale Self-Assembly Structures".</li>
+		<li>Primeaux, D. & Saunders, B., "Finding Global and Local Maxima in 3D Space Using Swarm-like Behavior in a Colony of Prehending Entities".</li>
+		<li>Primeaux, D., "Competing swarms of self-classifying entities with regional extension through feature space", Partial Draft.</li>
+	</ol>
+
+<?php require('../footer.php'); ?>
