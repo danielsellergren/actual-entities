ab2268bd286f4763467354c7b588335e45b84575
Daniel Sellergren <danielsellergren@gmail.com>
2013-11-11 17:56:32 -0500

    Add initial Welcome text

--- index.php
+++ index.php
@@ -2,4 +2,8 @@
 
     <h2>Welcome</h2>
 
+    <p>This site is intended to provide an introduction to and repository for information on the use of Actual Entities for Artificial Intelligence research and practice. There is a section on the history of Alfred North Whitehead's process philosophy and Actual Entities, an overview of how this theory translates into algorithms and objects in Artificial Intelligence, and a swarm demo with source code that can be analyzed.</p>
+
+    <p>Updates to the text will come when clarifications are requested or inaccuracies are identified. Updates to the demo will come when I have time to code new examples of how to solve problems or create worlds. Please contact the author, <a href="mailto:me@danielsellergren.com">Daniel Sellergren</a>, with any questions or comments.</p>
+
 <?php require('footer.php'); ?>
