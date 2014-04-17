ee2de8a1bcc1f6a468d1f809612f5ce480c6aa36
Daniel Sellergren <danielsellergren@gmail.com>
2014-04-17 19:52:45 -0400

    Adds copy to History page

--- history/index.php
+++ history/index.php
@@ -1,11 +1,85 @@
 <?php require('../header.php'); ?>
 
-    <h2>History of Actual Entities</h2>
+    <h2>A Quick History of Actual Entities</h2>
 
-    <p>Actual Entities are the fundamental elements of reality in the philosophy of English-American mathematician and logician, <a href="http://en.wikipedia.org/wiki/Alfred_North_Whitehead">Alfred North Whitehead</a>.</p>
+    <p>Actual Entities are the fundamental elements of reality in the philosophy of English-American mathematician and logician, <a href="http://en.wikipedia.org/wiki/Alfred_North_Whitehead">Alfred North Whitehead</a>. His philosophical worldview is known as <a href="http://en.wikipedia.org/wiki/Process_philosophy">Process Philosophy</a> and is based on the ideas of change and development.</p>
 
     <img src="../img/whitehead.jpg" width="285" height="320" alt="Alfred North Whitehead" />
 
-    <p>Whitehead is best known for his philosophical treatise <a href="http://en.wikipedia.org/wiki/Process_and_Reality">Process and Reality</a>, and for coauthoring the magnificent <a href="http://en.wikipedia.org/wiki/Principia_Mathematica">Principia Mathematica</a> with Bertrand Russell.</p>
+    <h3>Whitehead&rsquo;s Work</h3>
+
+    <p>Whitehead is best known for his philosophical treatise <a href="http://en.wikipedia.org/wiki/Process_and_Reality">Process and Reality</a>, and for coauthoring with Bertrand Russell the magnificent <a href="http://en.wikipedia.org/wiki/Principia_Mathematica">Principia Mathematica</a> which is considered by many to be the seminal work of modern symbolic logic.</p>
+
+    <p>In <em>Process and Reality</em>, Whitehead proposed that the universe is populated entirely by Actual Entities (AEs), Eternal Objects (EOs), and a principal object known as God. In this philosophical master work, all things are occasions of existence or "moments" and the process by which they change is the creative act of becoming.</p>
+
+    <img src="" />
+
+    <h3>Actual Entities</h3>
+
+    <h4>What is an Actual Entity?</h4>
+
+    <p>According to Whitehead, the universe does not consist of substances, but rather an abundance of processes which consist of "moments" or "occasions". These "occasions" are the actual entities themselves. Along with the AEs, the universe also consists of two other types known as Eternal Objects (EOs) and a non-temporal being termed God.</p>
+
+    <blockquote> An actual entity is a process of growth from phase to phase ending in a definite achievement. &mdash;Hooper</blockquote>
+
+    <p>The growth from phase to phase is called "concrescence" with the final result being termed "concretion".</p>
+
+    <img src="" />
+
+    <p>Additionally, actual entities are not everyday objects. Neither the books on your shelf, nor the people in your life are individual actual entities. Instead, picture yourself and everyone else as great "societies" of actual entities. A multitude of actual entities working as atomic elements, but also part of a functional whole. A single actual entity is much closer to a quantum vibration than an organism.</p>
+
+    <img src="" />
+
+    <h4>How is an Actual Entity created?</h4>
+
+    <p>So how are Actual Entities created? Each Actual Entity is a creative act, that looks at every other object in the universe and then builds a new and unique reflection of the universe. This reflection of the immediate settled state of the universe is itself the Actual Entity.</p>
+
+    <img src="" />
+
+    <p>We can think about an Actual Entity in terms of its two sides: the subject and the object.</p>
+
+    <p>The subject is the entity's immediate, private experience. When thinking about human beings this is generally thought of as consciousness, but it extends to a lesser extent in unconscious objects such as trees and rocks and then even further to the smallest societies of entities and their constituent occasions.</p>
+
+    <p>The object is the sum of the experiences of the subject. It is the external part of the entity which has the potential for being experienced by other entities in the universe. When a subject looks at an object, that object is reflected in the creation of the process. The experience of any given subject is dependent on the experiences of the object it is looking at.</p>
+
+    <img src="" />
+
+    <h3>The Doctrine of Prehensions</h3>
+
+    <h4>Causation</h4>
+
+    <p>We should first note two distinct types of causation that occur in the process philosophy. The way we normally think of causality in the physical world is here termed "efficient causation" because it follows directly from past states and events. Additionally, there is "final causation" which is the causality stemming from the push toward the end goals of the process. Efficient causation is the potentiality of the other actual entities becoming part of the formation of a new entity. Final causation is the self-creative process within the subject actual entity that makes decisions towards its goals based on the data available.</p>
+
+    <h4>Prehension</h4>
+
+    <p>Prehension is the act of a subject actual entity experiencing the data and experiences of all the object actual entities (and eternal objects), being changed via efficient causation and also changing itself via final causation. In other words, one entity looks at all the others and makes a decision as to how to modify itself based on what it sees and what it wants to be.</p>
+
+    <p>Think of a prehension as an operation with three steps:</p>
+
+    <ol>
+    	<li>Objectification of the data of the object entity.</li>
+    	<li>Interpretation of the data by the subject entity.</li>
+    	<li>Absorption of the data into the subject entity.</li>
+    </ol>
+
+    <img src="" />
+
+    <h4>Positive versus Negative Prehensions</h4>
+
+    <p>The prehensions discussed so far have all been positive prehensions. But there are also negative prehensions which mean that after the data has been objectified and interpreted, the subject entity deems that it is unimportant for development and rejects or ignores it. This decision may seem trivial, but not choosing to let something affect an entity is just as important as the decision to make changes.</p>
+
+    <h4>Eternal Objects</h4>
+
+    <p>The other objects in the universe other than actual entities are eternal objects, which do not prehend or have subjectivity, but can be prehended by each actual entity. The eternal object's purpose is to lend a definiteness to the actual entity, to provide it with a way of looking at the universe and interpreting what it sees. Essentially, the rules by which each actual entity makes the decision as to how an experience will affect them are determined by the definite eternal objects which were part of the cause of their initial novel synthesis.</p>
+
+    <h3>Use in Computer Science</h3>
+
+    <p>The philosophical concepts of Whitehead's process philosophy have been applied to several areas of artificial intelligence and machine learning. These areas include but are not limited to optimization problems, local minima and maxima, swarm theory, data clustering, genetic algorithms, and others. You can read much more about the applications of actual entities to artificial intelligence in the next section.</p>
+
+    <h3>References</h3>
+
+    <ol>
+    	<li>Hooper, S. E., "Whitehead's Philosophy: Actual Entities", *Philosophy*, Vol 16, No. 63, pp. 285-305.</li>
+    </ol>
 
 <?php require('../footer.php'); ?>
